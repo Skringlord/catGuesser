00d2aacde1b4fd39e4e2c9e42f512696e028da67
Artur Rukasuyeu <arturrukasuyeu@gmail.com>
2018-06-01 18:51:03 +0300

    3
    3

--- project.js
+++ project.js
@@ -5,11 +5,27 @@ const counter = document.querySelector('#guessBtn'),
       cats = document.querySelectorAll('#cat');
       
 var x = 0;
+var z = 0;
 cats.forEach(function(item, index){
+    
     item.addEventListener('click', function(e){
-        item.style.borderColor = 'orange';
+        colorCats();
+        if (item.style.borderColor === 'grey') {
+            item.style.borderColor = 'orange';
+            var choose = index;
+            console.log(`this chosen: ${choose}`);
+        } else {
+            item.style.borderColor = 'grey';
+         }
     });
 });
+
+function colorCats(){
+    cats.forEach(function(item){
+        item.style.borderColor = "grey";
+        console.log("do");
+    })
+}
 
 counter.addEventListener('click', function(e){
     x = x + 1;
