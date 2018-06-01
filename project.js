47dd8385af7699326a5573f269cf20ac964000e0
Artur Rukasuyeu <arturrukasuyeu@gmail.com>
2018-06-01 04:39:46 +0300

    3
    3

--- project.js
+++ project.js
@@ -1,8 +1,15 @@
 const counter = document.querySelector('#guessBtn'),
       attemptsAll = document.querySelector('#all-attempts');
-      attemptsCurrentSession = document.querySelector('#session-attempts');
+      attemptsCurrentSession = document.querySelector('#session-attempts'),
+      catsCards = document.querySelector('.cats-grid'),
+      cats = document.querySelectorAll('#cat');
       
 var x = 0;
+cats.forEach(function(item, index){
+    item.addEventListener('click', function(e){
+        item.style.borderColor = 'orange';
+    });
+});
 
 counter.addEventListener('click', function(e){
     x = x + 1;
@@ -12,4 +19,4 @@ counter.addEventListener('click', function(e){
 function setAtempts(){
     attemptsAll.textContent = (` Attempts made: ${x}`);
     attemptsCurrentSession.textContent = (` Attempts for this session: ${x}`)
-}
+};
